9e713cc224dd6ce6bddcb66cadb863afb7105429
Alex Dev Sm <leshasmlesha@gmail.com>
2022-05-17 13:37:08 +0300

    Поправил название

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,7 +4,7 @@ import { CourtModule } from './Court/Court.module';
 import { CreateExecModule } from './CreateExec/CreateExec.module';
 import { DeleteExecModule } from './DeleteExec/DeleteExec.module';
 import { DictModule } from './Dict/Dict.module';
-import { GetCommentModule } from './GetComment/getComment.module';
+import { GetCommentModule } from './GetComment/GetComment.module';
 import { LawExecModule } from './LawExec/LawExec.module';
 import { LoginModule } from './Login/Login.module';
 import { SearchModule } from './Search/Search.module';
