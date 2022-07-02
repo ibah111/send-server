efada3191fe67e52cd8acc853c99fd9b9a83fd0e
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-02 17:43:33 +0300

    Поменял путь

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ModelsModule } from './models';
 import { ModulesModule } from './Modules/Modules.module';
-import { PagesModule } from './pages';
+import { PagesModule } from './Pages';
 import { SmbModule } from './utils/smb';
 
 @Module({
