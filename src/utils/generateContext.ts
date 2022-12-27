298acb807288ce8c47a454a3e7bd3e8a3c19b182
Alex Dev Sm <leshasmlesha@gmail.com>
2022-12-27 09:59:36 +0300

    Поправил

--- src/utils/generateContext.ts
+++ src/utils/generateContext.ts
@@ -1,8 +1,9 @@
 export default function generateContext(user_id: number) {
-  return `DECLARE @BinVar varbinary(128);
-    DECLARE @StrVar nvarchar(128);
-    SET @StrVar = '${user_id};';
-    SET @BinVar =CAST(@StrVar as varbinary(128))
-    SET CONTEXT_INFO @BinVar;
-    GO;`;
+  return `
+  DECLARE @BinVar varbinary(128);
+  DECLARE @StrVar nvarchar(128);
+  SET @StrVar = '${user_id};';
+  SET @BinVar = CAST(@StrVar as varbinary(128));
+  SET CONTEXT_INFO @BinVar;
+  `;
 }
