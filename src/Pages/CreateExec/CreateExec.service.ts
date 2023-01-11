8300887f49403dbedf9adebfe8df8c2d763000da
Alex Dev Sm <leshasmlesha@gmail.com>
2023-01-11 16:41:24 +0300

    Поправил создание ИП

--- src/Pages/CreateExec/CreateExec.service.ts
+++ src/Pages/CreateExec/CreateExec.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@sql-tools/nestjs-sequelize';
 import { Injectable } from '@nestjs/common';
 import { AuthResult } from 'src/Modules/Guards/auth.guard';
 import { CreateExecInput } from './CreateExec.input';
+import { CreationAttributes } from '@sql-tools/sequelize';
 @Injectable()
 export class CreateExecService {
   constructor(
@@ -21,18 +22,21 @@ export class CreateExecService {
       if (work_task.length > 0)
         if (work_task[0].r_user_id !== null) user_id = work_task[0].r_user_id;
       if (la !== null) {
-        const le: LawExec = await la.$create('LawExec', {
+        const data: CreationAttributes<LawExec> = {
           r_person_id: la.r_person_id,
           r_debt_id: la.r_debt_id,
           r_portfolio_id: la.r_portfolio_id,
           state: 5,
           r_user_id: user_id,
+          load_dt: new Date(),
           DELIVERY_TYP: 3,
           contract: debt!.contract,
           currency: 1,
+          total_sum: 0,
           ...body.old,
           dsc: 'Создается ИП из "Отправка"',
-        });
+        };
+        const le: LawExec = await la.$create('LawExec', data);
         await le.$create('LawExecPersonLink', { PERSON_ID: la.r_person_id });
         await le.$create('LawExecProtokol', {
           r_user_id: auth.userContact.id,
