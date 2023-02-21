002f7fe1c1f79aa5f45153a2f73374c4cf0f0e6e
Alex Dev Sm <leshasmlesha@gmail.com>
2023-02-21 09:57:47 +0300

    Новый метод авторизации

--- src/Modules/Guards/check_login.ts
+++ src/Modules/Guards/check_login.ts
@@ -1,27 +1,7 @@
-import CryptoJS from 'crypto-js';
 import axios from 'axios';
-import { AuthUserError, AuthUserSuccess } from './auth.guard';
-import { UnauthorizedException } from '@nestjs/common';
+import { AuthUser, AuthUserError, AuthUserSuccess } from './auth.guard';
 import client from 'src/utils/client';
-const CryptoJSAesJson = {
-  stringify: function (cipherParams: CryptoJS.lib.CipherParams) {
-    const j: { ct?: string; iv?: string; s?: string } = {
-      ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64),
-    };
-    if (cipherParams.iv) j.iv = cipherParams.iv.toString();
-    if (cipherParams.salt) j.s = cipherParams.salt.toString();
-    return JSON.stringify(j);
-  },
-  parse: function (jsonStr: string) {
-    const j = JSON.parse(jsonStr);
-    const cipherParams = CryptoJS.lib.CipherParams.create({
-      ciphertext: CryptoJS.enc.Base64.parse(j.ct),
-    });
-    if (j.iv) cipherParams.iv = CryptoJS.enc.Hex.parse(j.iv);
-    if (j.s) cipherParams.salt = CryptoJS.enc.Hex.parse(j.s);
-    return cipherParams;
-  },
-};
+import server from 'src/utils/server';
 export const checkLogin = async (token: string) => {
   if (!token) {
     return false;
@@ -37,29 +17,9 @@ export const checkLogin = async (token: string) => {
           position: 'Должность',
         } as AuthUserSuccess)
       : ({ login_result: false } as AuthUserError);
-  let body = {};
-  try {
-    const encrypted = CryptoJS.enc.Base64.parse(token).toString(
-      CryptoJS.enc.Utf8,
-    );
-    const pass = CryptoJS.SHA512('Irjlf123!').toString();
-    body = JSON.parse(
-      CryptoJS.AES.decrypt(encrypted, pass, {
-        format: CryptoJSAesJson,
-      }).toString(CryptoJS.enc.Utf8),
-    );
-  } catch (err) {
-    throw new UnauthorizedException({
-      message: 'Не удалось прочитать токен доступа',
-      code: 'error_token',
-    });
-  }
-  const result = await axios.get(
-    'https://chat.nbkfinance.ru/scripts/login-api.php',
-    {
-      params: body,
-      headers: { 'accept-encoding': '*' },
-    },
+  const result = await axios.get<AuthUser<boolean>>(
+    server('oauth') + '/oauth/login',
+    { headers: { token } },
   );
   if (result.data === undefined || result.data === null) {
     return false;
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -3,23 +3,10 @@ export default (name = 'url') => {
   let prot;
   switch (name) {
     case 'token':
-      return config.token;
     case 'fastreport':
-      return config.fastreport;
+    case 'oauth':
+      return config[name];
     default:
-      switch (config.protocol) {
-        case 'http':
-          prot = 'http';
-          break;
-        case 'ssl':
-          prot = 'https';
-          break;
-        default:
-          prot = 'http';
-          break;
-      }
-      return `${prot}://${config.server}${
-        config.port === '80' ? '' : `:${config.port}`
-      }`;
+      return config.server;
   }
 };
