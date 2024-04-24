3f88533668f94cf43e4aba786d2e36e5f3051ca9
Ibah <starlord77@mail.ru>
2024-04-24 11:40:29 +0300

    chore(api): api variable description

--- src/Pages/Search/Search.input.ts
+++ src/Pages/Search/Search.input.ts
@@ -1,8 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
+const description = 'Search variable, it can be empty string';
 export class SearchInput {
+  @ApiProperty({
+    description,
+    default: '',
+  })
   @IsString()
   name: string;
   @IsString()
+  @ApiProperty({
+    description,
+    default: '',
+  })
   contract: string;
 }
