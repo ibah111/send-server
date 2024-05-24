584ec39c39e113c4315a7029e38924aa3463415a
Ibah <starlord77@mail.ru>
2024-05-24 10:21:30 +0300

    feat: changed endpoints and input class

--- src/Pages/Documents/Documents.input.ts
+++ src/Pages/Documents/Documents.input.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, ValidateIf } from 'class-validator';
 
-export class DocumentsInput {
+export class LawExecDocumentsInput {
+  @ApiProperty({
+    description: 'id для поиска вложений по исполнительному производству',
+  })
   @ValidateIf((o) => o.id === undefined)
   @IsNotEmpty()
   @IsNumber()
   law_exec_id: number;
+
+  @ApiProperty({
+    description: 'Я пока не знаю для чего это существует',
+  })
   @ValidateIf((o) => o.law_exec_id === undefined)
   @IsNotEmpty()
   @IsNumber()
   id: number;
+}
+
+export class LawActDocumentsInput {
+  @ApiProperty({
+    description: 'id для поиска вложений по исполнительному производству',
+  })
+  @IsNumber()
+  law_act_id: number;
 }
 export class DocumentsRemoveInput {
   @IsNotEmpty()
