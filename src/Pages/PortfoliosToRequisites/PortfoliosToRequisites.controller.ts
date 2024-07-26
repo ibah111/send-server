f69e4e5f2c409597f48722fa614794758ea50f7b
Ibah <starlord77@mail.ru>
2024-07-26 17:38:08 +0300

    feat: доделал эндпоинты

--- src/Pages/PortfoliosToRequisites/PortfoliosToRequisites.controller.ts
+++ src/Pages/PortfoliosToRequisites/PortfoliosToRequisites.controller.ts
@@ -1,8 +1,47 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import PortfoliosToRequisitesService from './PortfoliosToRequisites.service';
+import { IsNumber } from 'class-validator';
+
+class CreateLinkInput {
+  @ApiProperty({
+    required: true,
+  })
+  @IsNumber()
+  requisites_id: number;
+  @ApiProperty({
+    required: true,
+  })
+  @IsNumber()
+  portfolio_id: number;
+}
 
 @ApiTags('PortfoliosToRequisites')
 @Controller('PortfoliosToRequisites')
 export default class PortfoliosToRequisitesController {
-  constructor() {}
+  constructor(
+    private readonly portfolioService: PortfoliosToRequisitesService,
+  ) {}
+
+  @Get('getAllPortfolio')
+  getAllPortfolio() {
+    return this.portfolioService.getAllPortfolio();
+  }
+
+  @Get('getAllLinksByRequisites/:id')
+  getAllLinksByRequisites(@Param(':id') requisites_id: number) {
+    return this.portfolioService.getAllLinksByRequisites(requisites_id);
+  }
+
+  @Get('getRequisitesByPortfolio/:id')
+  getRequisitesByPortfolio(@Param(':id') portfolio_id: number) {
+    return this.portfolioService.getRequisitesByPortfolio(portfolio_id);
+  }
+
+  @Post('createPortfolioToRequisitesLink')
+  createPortfolioToRequisitesLink(@Body() body: CreateLinkInput) {
+    return this.portfolioService.createPortfolioToRequisitesLink({
+      ...body,
+    });
+  }
 }
