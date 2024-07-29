316ea61c28870edc3c3e079bcc28755b71e3d806
Ibah <starlord77@mail.ru>
2024-07-29 13:45:48 +0300

    feat: изменил экспорт сервиса на экспорт модуля

--- src/Pages/Exec/UpdateExec/UpdateExec.module.ts
+++ src/Pages/Exec/UpdateExec/UpdateExec.module.ts
@@ -5,11 +5,11 @@ import { DownloaderModule } from 'src/utils/downloader';
 import { HelperModule } from 'src/utils/helper';
 import { UpdateExecController } from './UpdateExec.controller';
 import { UpdateExecService } from './UpdateExec.service';
-import PortfoliosToRequisitesService from 'src/Pages/PortfoliosToRequisites/PortfoliosToRequisites.service';
+import PortfoliosToRequisitesModule from 'src/Pages/PortfoliosToRequisites/PortfoliosToRequisites.module';
 
 @Module({
   imports: [
-    PortfoliosToRequisitesService,
+    PortfoliosToRequisitesModule,
     DownloaderModule,
     HelperModule,
     SequelizeModule.forFeature([LawExec, DocAttach], 'contact'),
