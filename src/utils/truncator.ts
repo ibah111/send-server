fae7aef8b2d5eaaad10b8352ea84d62f71c330e3
Ibah <starlord77@mail.ru>
2024-10-28 14:06:43 +0300

    feat: add error to truncator function, and deleting 100 characters initially

--- src/utils/truncator.ts
+++ src/utils/truncator.ts
@@ -1,7 +1,7 @@
 export default function truncator(value: string) {
   const length = value.length;
   if (length >= 2000) {
-    let sum_length: number = 0;
+    let sum_length: number = 100;
     const arr = value.split('\n');
     const split_arr = arr.reduce((prev, curr) => prev + `${curr}`);
     const over_length = split_arr.length;
@@ -11,6 +11,10 @@ export default function truncator(value: string) {
       sum_length += item.length;
     }
     const a = split_arr.slice(sum_length);
-    return a;
+    console.log(`a.length: ${a.length}`, 'truncator value: ', a);
+    if (a.length >= 2000) {
+      console.log('values ', a);
+      throw Error('Укоротите сообщение');
+    } else return a;
   } else return value;
 }
