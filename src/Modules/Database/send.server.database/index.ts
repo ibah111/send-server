6be8ab7001f75a6e6069b8c241c92129d9308a99
ibah <starlord77@mail.ru>
2025-02-27 17:55:29 +0300

    feat: db connections

--- src/Modules/Database/send.server.database/index.ts
+++ src/Modules/Database/send.server.database/index.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@sql-tools/nestjs-sequelize';
 import { ServerLocalModels } from './server.models';
 import { LocalServerDatabaseSeed } from './seed';
+import { node } from 'src/main';
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
-      host: 'newct.usb.ru',
+      host: node === 'prod' ? 'newct.usb.ru' : 'balezin.usb.ru',
       dialect: 'mssql',
       name: 'send',
-      username: 'contact',
-      password: 'contact',
+      username: node === 'prod' ? 'contact' : 'admin',
+      password: node === 'prod' ? 'contact' : 'admin1',
       database: 'Send',
       logging: false,
       models: ServerLocalModels,
